c5fda1cf9c25e1d57bb9f38ac6b2fb7b763f6b0e
oJaciel <jacielhbarichellO@gmail.com>
2025-11-22 09:55:48 -0300

    first commit

---
+++ lib/models/field.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/foundation.dart';
+import 'explosion_exception.dart';
+
+class Field {
+  final int line;
+  final int column;
+  final List<Field> neighbors = [];
+
+  bool _open = false;
+  bool _marked = false;
+  bool _mined = false;
+  bool _exploded = false;
+
+  Field({required this.line, required this.column});
+
+  void addNeighbor(Field neighbor) {
+    final deltaLine = (line - neighbor.line).abs();
+    final deltaColumn = (column - neighbor.column).abs();
+
+    if (deltaLine == 0 && deltaColumn == 0) {
+      return;
+    }
+
+    if (deltaLine <= 1 && deltaColumn <= 1) {
+      neighbors.add(neighbor);
+    }
+  }
+
+  void open() {
+    if (_open) return;
+
+    _open = true;
+
+    if (_mined) {
+      _exploded = true;
+      throw ExplosionException();
+    }
+
+    if (secureNeighborhood) {
+      neighbors.forEach((v) => v.open());
+    }
+  }
+
+  bool get mined {
+    return _mined;
+  }
+
+  bool get secureNeighborhood {
+    return neighbors.every((n) => n.mined == false);
+  }
+}
