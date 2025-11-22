5fc09bd3fe4fc058f5f48adbe9104ea7bdf39413
oJaciel <jacielhbarichellO@gmail.com>
2025-11-22 10:11:41 -0300

    Model Field
    Implementado modelo para os campos do jogo.

--- lib/models/field.dart
+++ lib/models/field.dart
@@ -1,51 +1,111 @@
-import 'package:flutter/foundation.dart';
 import 'explosion_exception.dart';
 
 class Field {
+  Field({required this.line, required this.column});
+
+  //Linha e coluna que está o campo
   final int line;
   final int column;
+
+  //Lista de campos ao redor desse
   final List<Field> neighbors = [];
 
-  bool _open = false;
+  //Booleanos para controle do jogo
+  bool _opened = false;
   bool _marked = false;
   bool _mined = false;
   bool _exploded = false;
 
-  Field({required this.line, required this.column});
-
+  //Adiciona vizinho ao campo
   void addNeighbor(Field neighbor) {
+    //Primeiro verifica o delta da linha e coluna do campo com o outro
     final deltaLine = (line - neighbor.line).abs();
     final deltaColumn = (column - neighbor.column).abs();
 
+    //Se o resultado for 0 em ambos, é porque o campo selecionado é o mesmo que está sendo comparado
+    //Somente ignora
     if (deltaLine == 0 && deltaColumn == 0) {
       return;
     }
 
+    //Se a diferença de ambos for somente 1, é porque o campo selecionado está do lado do atual
     if (deltaLine <= 1 && deltaColumn <= 1) {
+      //Adiciona campo a lista de vizinhos
       neighbors.add(neighbor);
     }
   }
 
+  //Abrir um campo
   void open() {
-    if (_open) return;
+    //Se já está aberto, não faz nada
+    if (_opened) return;
 
-    _open = true;
+    _opened = true;
 
+    //Se o campo aberto está minado, perde
     if (_mined) {
       _exploded = true;
       throw ExplosionException();
     }
 
+    //Se ele for aberto, e a vizinhança não estiver minada, segue abrindo outros campos
     if (secureNeighborhood) {
       neighbors.forEach((v) => v.open());
     }
   }
 
+  //Fim do jogo
+  //Se o campo estiver minado, abre ele
+  void revealBomb() {
+    if (_mined) _opened = true;
+  }
+
+  //Settando um campo como minado
+  void mineField() {
+    _mined = true;
+  }
+
+  //Alternando a marcação do campo
+  void toggleMark() {
+    _marked = !_marked;
+  }
+
+  //Reinicia o jogo
+  void restart() {
+    _opened = false;
+    _marked = false;
+    _mined = false;
+    _exploded = false;
+  }
+
+  //Getters dos parâmetros
   bool get mined {
     return _mined;
   }
 
+  bool get exploded {
+    return _exploded;
+  }
+
+  bool get opened {
+    return _opened;
+  }
+
+  bool get marked {
+    return _marked;
+  }
+
+  bool get resolved {
+    bool minedAndMarked = mined && marked;
+    bool secureAndOpened = !mined && opened;
+    return minedAndMarked || secureAndOpened;
+  }
+
   bool get secureNeighborhood {
     return neighbors.every((n) => n.mined == false);
+  }
+
+  int get neighborhoodMineQuantity {
+    return neighbors.where((n) => n.mined).length;
   }
 }
