70ad35633b02dd53ce56ef110584c1a15bf1ce17
oJaciel <jacielhbarichellO@gmail.com>
2025-11-24 10:25:05 -0300

    Lógica do Jogo
    Implementada a lógica do jogo.

--- lib/screens/minesweeper.dart
+++ lib/screens/minesweeper.dart
@@ -1,18 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:minesweeper/components/board_widget.dart';
 import 'package:minesweeper/components/result_widget.dart';
+import 'package:minesweeper/models/board.dart';
+import 'package:minesweeper/models/explosion_exception.dart';
+import 'package:minesweeper/models/field.dart';
 
-class Minesweeper extends StatelessWidget {
+class Minesweeper extends StatefulWidget {
   const Minesweeper({super.key});
 
+  @override
+  State<Minesweeper> createState() => _MinesweeperState();
+}
+
+class _MinesweeperState extends State<Minesweeper> {
+  bool? _winned;
+  Board _board = Board(lines: 12, columns: 12, bombQuantity: 3);
+
   _restart() {
-    print('Reiniciar');
+    setState(() {
+      _winned = null;
+      _board.restart();
+    });
+  }
+
+  _open(Field field) {
+    if (_winned != null) {
+      return;
+    }
+    setState(() {
+      try {
+        field.open();
+        if (_board.resolved) {
+          _winned = true;
+        }
+      } on ExplosionException {
+        _winned = false;
+        _board.revealBombs();
+      }
+    });
+  }
+
+  _toggleMark(Field field) {
+    if (_winned != null) {
+      return;
+    }
+    setState(() {
+      field.toggleMark();
+      if (_board.resolved) {
+        _winned = true;
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: ResultWidget(winned: true, onRestart: _restart),
-      body: Container(child: Text('Tabuleiro')),
+      appBar: ResultWidget(winned: _winned, onRestart: _restart),
+      body: BoardWidget(
+        board: _board,
+        onOpen: _open,
+        onToggleMark: _toggleMark,
+      ),
     );
   }
 }
